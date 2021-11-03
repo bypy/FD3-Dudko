d326368f5fc03068bf29e8d367e037050c1922e7
alexey.dudko <leva@mail.com>
2021-11-04 01:12:10 +0300

    feat: add item selection handlers

--- 02/ishop/components/IShop.js
+++ 02/ishop/components/IShop.js
@@ -2,6 +2,10 @@ const IShop = React.createClass({
 
     displayName: "IShop",
 
+    getDefaultProps: () => {
+        return {selectedItem: 2};
+    },
+
     getHeader: (shopCardProperties) => {
         return React.DOM.div({className: "item-properties"},
             shopCardProperties.map((property, index) => React.DOM.div(
@@ -11,23 +15,30 @@ const IShop = React.createClass({
         );
     },
 
-    getRecords: (shopCardRecords) => {
+    getRecords: (shopCardRecords, selectedItem, focusChangeCb) => {
         return shopCardRecords.map(cardRecord => React.createElement(IShopItem, {
             key: cardRecord.id,
+            id: cardRecord.id,
             name: cardRecord.name,
             price: cardRecord.price,
             currency: cardRecord.currency,
             url: cardRecord.url,
             quantity: cardRecord.quantity,
             details: cardRecord.details,
+            focus: selectedItem,
+            onFocusChangeCb: focusChangeCb,
         }))
+    },
+
+    focusChangeCb: function(itemId) {
+        this.props.selectedItem = itemId;
     },
 
     render: function () {
         return React.DOM.div({className: "IShop"},
             React.DOM.div({className: "caption"}, this.props.caption),
             this.getHeader(this.props.headline),
-            this.getRecords(this.props.records)
+            this.getRecords(this.props.records, this.props.selectedItem, this.focusChangeCb)
         )
     }
 })
--- 02/ishop/components/IShopItem.js
+++ 02/ishop/components/IShopItem.js
@@ -2,16 +2,27 @@ const IShopItem = React.createClass({
     displayName: "IShopItem",
 
     propTypes: {
+        id: React.PropTypes.number.isRequired,
         name: React.PropTypes.string.isRequired,
         price: React.PropTypes.number.isRequired,
         currency: React.PropTypes.string.isRequired,
         url: React.PropTypes.string.isRequired,
         quantity: React.PropTypes.number.isRequired,
         details: React.PropTypes.string.isRequired,
+        focus: React.PropTypes.number,
+        onFocusChangeCb: React.PropTypes.func,
+    },
+
+    changeFocusHandler: function(EO) {
+        if (this.props.onFocusChangeCb)
+            this.props.onFocusChangeCb(this.props.id);
     },
 
     render: function () {
-        return React.DOM.div({className: "IShopItem"},
+        let className = (this.props.id !== this.props.focus)
+            ? "IShopItem"
+            : "IShopItem selected";
+        return React.DOM.div({className: className, onClick: this.changeFocusHandler},
             React.DOM.div(null, this.props.name),
             React.DOM.div(null, this.props.price),
             React.DOM.div(null, this.props.currency),
