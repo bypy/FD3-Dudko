c99717100e91a54f867101f5ef1cf3f6910a5824
Alexey Dudko <js.dudko@gmail.com>
2021-11-06 11:09:53 +0300

    switch to state

--- 02/filter/components/Filter.js
+++ 02/filter/components/Filter.js
@@ -1,14 +1,17 @@
 const Filter = React.createClass({
   displayName: "FilterComponent",
 
-  getOptions() {
-    return this.props.words.map((word, index) => React.DOM.option({ key: btoa(word), value: index }, word));
+  getInitialState() {
+    return ({
+      words: this.props.words.map(
+        (word, index) => React.DOM.option({ key: btoa(word), value: index }, word))
+    })
   },
 
-  render: function () {
+  render() {
     return React.DOM.select(
-      { className: "Filter", size: (this.props.words.length).toString(), name: "my-select-component" },
-      this.getOptions()
+      { className: "Filter", size: (this.state.words.length).toString(), name: "display-area" },
+      this.state.words
     )
   }
 })
