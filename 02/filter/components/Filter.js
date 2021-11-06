33f212b2cae001076a9459047906eb7a4cfe9921
Alexey Dudko <js.dudko@gmail.com>
2021-11-06 19:10:52 +0300

    add filtering

--- 02/filter/components/Filter.js
+++ 02/filter/components/Filter.js
@@ -1,21 +1,37 @@
 const Filter = React.createClass({
   displayName: "FilterComponent",
 
-  getInitialState: function() {
+  getInitialState() {
     return {
-      words: this.props.words.map(
-        (word, index) => React.DOM.option({ key: btoa(word), value: index }, word)).sort(),
-      isSort: this.props.isSort,
+      words: this.props.words.filter(w => true), // create shallow copy of props.word array
+      isSort: this.props.isSort || false,
+      filterString: this.props.filterString || "",
     }
   },
 
-  toggleSort: function(EO) {
-    console.log(this.state.isSort);
-    console.log(EO.target.checked);
+  comparatorFunc(curr, next) {
+    if (curr === next) return 0;
+    else return (curr.toLowerCase() < next.toLowerCase()) ? -1 : 1;
+  },
+  
+  toggleSort(EO) {
     this.setState({ isSort: EO.target.checked });
   },
 
-  render: function() {
+  applyFilter(EO) {
+    this.setState({ filterString: EO.target.value });
+  },
+
+  getListVdom() {
+    let list = this.state.isSort ? this.state.words.sort(this.comparatorFunc) : this.props.words;
+    const filterString = this.state.filterString.trim();
+    if (filterString.length > 0) {
+      list = list.filter(w => w.indexOf(this.state.filterString) !== -1);
+    }
+    return list.map((word, index) => React.DOM.option({ key: word, value: index }, word));
+  },
+
+  render() {
     return React.DOM.div({ className: "Filter" },
       React.DOM.input(
         {
@@ -23,10 +39,15 @@ const Filter = React.createClass({
           onChange: this.toggleSort
         }
       ),
+      React.DOM.input(
+        {
+          className: "filter", type: "text", value: this.state.filterString,
+          onChange: this.applyFilter
+        }
+      ),
       React.DOM.select(
         { className: "display", size: (this.state.words.length).toString(), name: "display" },
-        // this.state.isSort ? this.state.words.sort() : this.state.words
-        this.state.words
+        this.getListVdom(),
       )
     )
   }
