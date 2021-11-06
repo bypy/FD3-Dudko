c4f992d241f916ecc38f6efc0a9fe43ccb36846f
Alexey Dudko <js.dudko@gmail.com>
2021-11-06 12:50:55 +0300

    snapshot

--- 02/filter/components/Filter.js
+++ 02/filter/components/Filter.js
@@ -1,17 +1,33 @@
 const Filter = React.createClass({
   displayName: "FilterComponent",
 
-  getInitialState() {
-    return ({
+  getInitialState: function() {
+    return {
       words: this.props.words.map(
-        (word, index) => React.DOM.option({ key: btoa(word), value: index }, word))
-    })
+        (word, index) => React.DOM.option({ key: btoa(word), value: index }, word)).sort(),
+      isSort: this.props.isSort,
+    }
   },
 
-  render() {
-    return React.DOM.select(
-      { className: "Filter", size: (this.state.words.length).toString(), name: "display-area" },
-      this.state.words
+  toggleSort: function(EO) {
+    console.log(this.state.isSort);
+    console.log(EO.target.checked);
+    this.setState({ isSort: EO.target.checked });
+  },
+
+  render: function() {
+    return React.DOM.div({ className: "Filter" },
+      React.DOM.input(
+        {
+          className: "sort", type: "checkbox", checked: this.state.isSort,
+          onChange: this.toggleSort
+        }
+      ),
+      React.DOM.select(
+        { className: "display", size: (this.state.words.length).toString(), name: "display" },
+        // this.state.isSort ? this.state.words.sort() : this.state.words
+        this.state.words
+      )
     )
   }
 })
